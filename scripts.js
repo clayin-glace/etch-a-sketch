a30b3a19a82057a190b1d9daf2747ecb0445de7f
hw11chung <ddoldolry@gmail.com>
2023-01-12 04:30:30 -0800

    assign id numbers to grid squares

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 console.log(container);
 
 for(i = 0; i < 256; i++) {
-  var square = document.createElement('div');
+  var square = document.createElement("div");
   square.className = "square";
+  square.id = i;
 
-  document.querySelector('#container').appendChild(square);
+  document.querySelector("#container").appendChild(square);
+}
+
+var hover = document.getElementsByClassName("square").addEventListener("mouseover", changeColor);
+
+function changeColor(e) {
+  console.log(e.target);
 }
