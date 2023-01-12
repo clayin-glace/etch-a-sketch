95f5971d598dd26412865b6b526689ff13c3f0fd
hw11chung <ddoldolry@gmail.com>
2023-01-12 02:12:34 -0800

    add square divs using a for loop in js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('#container');
 console.log(container);
-const div = document.createElement('div');
 
+for(i = 0; i < 5; i++) {
+  var square = document.createElement('div');
+  square.className = "square";
 
+  document.querySelector('#container').appendChild(square);
+}
